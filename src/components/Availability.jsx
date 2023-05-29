f2483c3375da9dd68f50cefa207339fe5192ddcc
Gitam <gitam.shimoni@gmail.com>
2023-05-29 16:16:22 +0300

    after rebase

---
+++ src/components/Availability.jsx
@@ -0,0 +1,54 @@
+import "./availability.css";
+const Availability = () => {
+  return (
+    <div id="availble-main-div">
+      <h1></h1>
+      <table id="availble-table">
+        <tbody>
+          <tr>
+            <td>Day / Availble</td>
+            <td>Sunday</td>
+            <td>Monday</td>
+            <td>Tuesday</td>
+            <td>Wednsday</td>
+            <td>Thursday</td>
+            <td>Friday</td>
+            <td>Saturday</td>
+          </tr>
+          <tr>
+            <td>Morning</td>
+            <td>hi</td>
+            <td>hi</td>
+            <td>hi</td>
+            <td>hi</td>
+            <td>hi</td>
+            <td>hi</td>
+            <td>hi</td>
+          </tr>
+          <tr>
+            <td>Lunch</td>
+            <td>hi</td>
+            <td>hi</td>
+            <td>hi</td>
+            <td>hi</td>
+            <td>hi</td>
+            <td>hi</td>
+            <td>hi</td>
+          </tr>
+          <tr>
+            <td>Evening</td>
+            <td>hi</td>
+            <td>hi</td>
+            <td>hi</td>
+            <td>hi</td>
+            <td>hi</td>
+            <td>hi</td>
+            <td>hi</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Availability;
